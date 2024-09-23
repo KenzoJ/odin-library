728273495de6e41bdbb07e0e38340290ec3cb13c
KenzoJ <charles.felux@gmail.com>
2024-09-22 21:40:10 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const myLibrary = [
+        "Great Gumble",
+        "Laws of propane",
+        "Suzanne's diary"
+    ];
+
+    showLibrary();
+
+
+    function showLibrary() {
+        const libraryContainer = document.getElementById('books');
+        libraryContainer.innerHTML = myLibrary.map(book => `<div>${book}</div>`).join('');
+    }
+});
+
+
+function Book() {
+    // the constructor...
+  };
+  
+      function addBookToLibrary() {
+    // do stuff here
+  };
